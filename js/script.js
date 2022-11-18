511fef2cdb35c825d79d04902b7b8809e755e8f9
cerry24 <samuelecerretti@gmail.com>
2022-11-18 16:01:06 +0100

    implement js print output

--- js/script.js
+++ js/script.js
@@ -5,3 +5,6 @@ let userLastName = prompt('Inserisci il tuo cognome');
 let favouriteColor = prompt('Inserisci il tuo colore preferito');
 
 let password = userName + userLastName + favouriteColor;
+
+
+document.getElementById('output').innerHTML = `La tua nuova password &egrave ${password}`;
