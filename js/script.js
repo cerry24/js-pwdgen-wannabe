b445aa86493fec5afd25f067634326baeeda4963
cerry24 <samuelecerretti@gmail.com>
2022-11-18 16:00:34 +0100

    declare js variables

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let userName = prompt('Inserisci il tuo nome');
+
+let userLastName = prompt('Inserisci il tuo cognome');
+
+let favouriteColor = prompt('Inserisci il tuo colore preferito');
+
+let password = userName + userLastName + favouriteColor;
